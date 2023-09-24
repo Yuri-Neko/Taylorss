73d3a4166b2b2ab3e140dcbbda90f77930a7bbf3
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2023-09-24 19:57:28 +0800

    Add files via upload

--- plugins/verify-email-hint.js
+++ plugins/verify-email-hint.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn }) => {
-    conn.emailotp = conn.emailotp ? conn.emailotp : {}
+    conn.regmail = conn.regmail ? conn.regmail : {}
     let id = m.chat
-    if (!(id in conn.emailotp)) throw false
-    let json = conn.emailotp[id][1]
+    if (!(id in conn.regmail)) throw false
+    let json = conn.regmail[id][1]
     conn.reply(m.chat, '```' + json.jawaban.replace(/[AIUEOaiueo]/ig, '_') + '```', m)
 }
 handler.command = /^hotp$/i
